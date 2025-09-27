c39219d8699f5f06f23287fd5054e56221f3614e
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-09-27 21:41:05 +0800

    chore(package): ignored prisma client output

--- apps/backend/eslint.config.mjs
+++ apps/backend/eslint.config.mjs
@@ -1,10 +1,11 @@
 // eslint.config.mjs
 // @ts-check
 import eslint from "@eslint/js";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 
-export default defineConfig(
+export default defineConfig([
+  globalIgnores(["node_modules", "dist", "generated/prisma/**"]),
   eslint.configs.recommended,
-  tseslint.configs.recommended
-);
+  tseslint.configs.recommended,
+]);
