7b590f3c3c9ba75de09bfc4d32de41162752ef8b
nethangabrielb <112095627+suix0@users.noreply.github.com>
2025-09-28 11:22:41 +0800

    feat: user registration

--- apps/backend/src/controllers/guest/authController.ts
+++ apps/backend/src/controllers/guest/authController.ts
@@ -1,13 +1,42 @@
 import { Request, Response } from 'express';
 
-import type { ApiResponse } from '../../types/apiResponse';
+import bcrypt from 'bcryptjs';
+
+import { prisma } from '../../lib/prisma';
 import type { RegistrationBody } from '../../types/registrationBody';
 
 const authController = (() => {
-  const register = (
+  const register = async (
     req: Request<object, object, RegistrationBody>,
     res: Response
-  ) => {};
+  ) => {
+    try {
+      const { name, username, email, password } = req.body;
+
+      const encryptedPassword = await bcrypt.hash(password, 10);
+
+      const newUser = await prisma.user.create({
+        data: {
+          name,
+          username,
+          email,
+          password: encryptedPassword,
+        },
+      });
+
+      res.json({
+        status: 'success',
+        message: 'User created successfully!',
+        data: newUser,
+      });
+    } catch (err) {
+      res.json({
+        status: 'error',
+        message: 'An error occured in the server. Please try again.',
+        data: err,
+      });
+    }
+  };
 
   const login = (req: Request, res: Response) => {};
 
--- apps/backend/src/types/apiResponse.ts
+++
@@ -1,7 +0,0 @@
-interface ApiResponse<T> {
-  status: "success" | "error";
-  data?: T;
-  message?: string;
-}
-
-export type { ApiResponse };
