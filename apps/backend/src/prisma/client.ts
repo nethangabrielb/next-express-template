1ac72c9a5660e0907acec650b1362486db334008
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-03-19 12:02:52 +0800

    chore(db): migrate prisma 6 to prisma 7

--- apps/backend/src/prisma/client.ts
+++ apps/backend/src/prisma/client.ts
@@ -1,3 +1,5 @@
-import { PrismaClient } from "../../generated/prisma/client.js";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client/extension';
 
-export const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+export const prisma = new PrismaClient({ adapter });
